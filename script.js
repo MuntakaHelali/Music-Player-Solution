d2b46a0579bdd23ae00f01f7725a69876ace6df5
MuntakaH <57158277+MuntakaHelali@users.noreply.github.com>
2024-04-01 23:14:30 -0400

    Step 55 - 65 Solution
    Implemented functions in JavaScript to highlight current song playing in the playlist, as well as displaying the song title and artist within the music player display.

--- script.js
+++ script.js
@@ -95,6 +95,8 @@ const playSong = (id)=>{
   }
   userData.currentSong = song;
   playButton.classList.add("playing");
+  highlightCurrentSong();
+  setPlayerDisplay();
   audio.play();
 };
 
@@ -124,6 +126,26 @@ const playPreviousSong = ()=>{
     const previousSong = userData?.songs[currentSongIndex-1];
     playSong(previousSong.id);
   }
+};
+
+const setPlayerDisplay = ()=>{
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+  playingSong.textContent = currentTitle ?  currentTitle: "";  
+  songArtist.textContent = currentArtist ?  currentArtist: "";  
+};
+
+const highlightCurrentSong = ()=>{
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+  playlistSongElements.forEach((songEl)=>{
+    songEl.removeAttribute("aria-current");
+    if(songToHighlight){
+      songToHighlight.setAttribute("aria-current", "true");
+    }
+  })
 };
 
 const renderSongs = (array)=>{
