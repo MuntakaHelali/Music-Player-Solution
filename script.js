c8c53aba43cd825efa49ea15c3427351691b02e5
MuntakaH <57158277+MuntakaHelali@users.noreply.github.com>
2024-04-11 21:47:25 -0400

    Steps 86 - 99 Solution
    Finished the reset playlist functionality and fixed the issue with song playback once a song ends.

--- script.js
+++ script.js
@@ -158,6 +158,12 @@ const deleteSong = (id)=> {
     resetButton.ariaLabel = "Reset playlist";
     resetButton.appendChild(resetText);
     playlistSongs.appendChild(resetButton);
+    resetButton.addEventListener("click", ()=>{
+      userData.songs = [...allSongs];
+      renderSongs(sortSongs());
+      setPlayButtonAccessibleText();
+      resetButton.remove();
+    });
   }
 };
 
@@ -222,6 +228,22 @@ nextButton.addEventListener("click", playNextSong);
 previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
+
+audio.addEventListener("ended", ()=>{
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = (userData.songs.length-1) > currentSongIndex;
+  if(nextSongExists){
+    playNextSong();
+  }
+  else{
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    pauseSong();
+    setPlayerDisplay();
+    highlightCurrentSong();
+    setPlayButtonAccessibleText();
+  }
+});
 
 const sortSongs = () => {
   userData?.songs.sort((a, b)=>{
